893f9ef978c06dc96e4151f757346ced13bfc342
Niceno <bojan.niceno.scientist@gmail.com>
2025-06-24 18:46:14 +0530

    Add migration and superuser

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ pip install -r requirements.txt
 echo "Running migrations..."
 python manage.py migrate
 
+echo "Creating superuser..."
+python create_superuser.py  # Make sure this file exists and works
+
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
-
-echo "Creating superuser..."
-python create_superuser.py
