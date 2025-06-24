1687865afa38246ff354757915b1e67f0ce2d2e6
Niceno <bojan.niceno.scientist@gmail.com>
2025-06-24 17:20:43 +0530

    Add superuser creation to build script

--- build.sh
+++ build.sh
@@ -8,3 +8,6 @@ python manage.py migrate
 
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
+
+echo "Creating superuser..."
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'adminpass')" | python manage.py shell
