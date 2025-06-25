3a82d655f4bf5f0d80f49fafc3a0ef1afd256baa
Niceno <bojan.niceno.scientist@gmail.com>
2025-06-25 12:35:54 +0530

    Added create_superuser to build

--- create_superuser.py
+++ create_superuser.py
@@ -1,10 +1,12 @@
-import os
-import django
+# import os
+# import django
+
 from django.contrib.auth import get_user_model
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todoproject.settings")
-django.setup()
-
 User = get_user_model()
+
 if not User.objects.filter(username="vishal").exists():
     User.objects.create_superuser("vishal", "alphaz010101@gmail.com", "todoapp11")
+    print("Superuser created.")
+else:
+    print("Superuser already exists.")
