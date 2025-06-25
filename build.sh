e00965e7ad5bcb2a44c08e3b82382336ba46c19f
Niceno <bojan.niceno.scientist@gmail.com>
2025-06-25 14:14:16 +0530

    Trigger redeploy

--- build.sh
+++ build.sh
@@ -12,5 +12,3 @@ python create_superuser.py  # Make sure this file exists and works
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); u=User.objects.get(username='vishal'); u.is_staff=True; u.is_superuser=True; u.save()" | python manage.py shell
-
